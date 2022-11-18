1c8fe77d80ae8f06b695aa1ec1441a9dfd04a751
Laure-Riglet <laure.riglet@gmail.com>
2022-11-18 15:28:16 +0100

    OK simple static cards

--- js/timer.js
+++ js/timer.js
@@ -4,11 +4,17 @@ const timer = {
     targetTime: Math.floor(Date.parse('14 Mar 2023 09:00:00 GMT+0100') / 1000),
 
     init: function () {
+
         setInterval(timer.updateTimeRemaining, 1000);
-        setInterval(timer.displayDaysRemaining, 1000);
+
+        /* setInterval(timer.displayDaysRemaining, 1000);
         setInterval(timer.displayHoursRemaining, 1000);
-        setInterval(timer.displayMinutesRemaining, 1000);
+        setInterval(timer.displayMinutesRemaining, 1000); */
         setInterval(timer.displaySecondsRemaining, 1000);
+
+        setInterval(timer.swapCards, 1000);
+        setInterval(timer.waitThenMoveActiveDownCards, 500);
+
     },
 
     updateTimeRemaining: function () {
@@ -18,7 +24,9 @@ const timer = {
         return timer.targetTime - currentTime;
     },
 
-    // Getters 
+    //**********************************************//
+    //                   GETTERS                    //
+    //**********************************************//
 
     getDaysRemaining: function () {
         // on retourne le nombre de jours entiers jusqu'à la date et l'heure visées,
@@ -59,7 +67,9 @@ const timer = {
         return secondsToGo;
     },
 
-    // Setters
+    //**********************************************//
+    //                   SETTERS                    //
+    //**********************************************//
 
     displayDaysRemaining: function () {
         // on crée une variable qui récupère le nombre de jours à afficher
@@ -76,7 +86,7 @@ const timer = {
             }
             // on injecte le nombre de jours entiers jusqu'à la date et l'heure visées
             daysFigure[0].textContent = daysToDisplay;
-            daysFigure[1].textContent = daysToDisplay;
+            daysFigure[3].textContent = daysToDisplay;
         }
     },
 
@@ -95,7 +105,7 @@ const timer = {
             }
             // on injecte le nombre d'heures entières jusqu'à la date et l'heure visées
             hoursFigure[0].textContent = hoursToDisplay;
-            hoursFigure[1].textContent = hoursToDisplay;
+            hoursFigure[3].textContent = hoursToDisplay;
         }
     },
 
@@ -112,31 +122,102 @@ const timer = {
             if (minutesToDisplay.length < 2) {
                 minutesToDisplay = '0' + minutesToDisplay;
             }
-            // on injecte le nombre de minutes entières jusqu'à la date et l'minute visées
+            // on injecte le nombre de minutes entières jusqu'à la date et l'heure visées
             minutesFigure[0].textContent = minutesToDisplay;
-            minutesFigure[1].textContent = minutesToDisplay;
+            minutesFigure[3].textContent = minutesToDisplay;
         }
     },
 
     displaySecondsRemaining: function () {
-        // on crée une variable qui récupère le nombre de secondes à afficher
-        let secondsToDisplay = timer.getSecondsRemaining().toString();
+        
+        // on crée une variable qui récupère le nombre courant de secondes restantes 
+        // à afficher
+        let secondsToDisplayUpInact = timer.getSecondsRemaining();
+        
+        // on cree également les variables des cartes qui seront en attente 
+        // sur la partie haute et qui subsisteront sur la partie basse
+        let secondsToDisplayUpAct = secondsToDisplayUpInact - 1;
+        let secondsToDisplayDownAct = secondsToDisplayUpInact; // + 10 aucune influence...
+        let secondsToDisplayDownActMove = secondsToDisplayUpInact - 2;
+
+        // on les converti en string afin de pouvoir les mettre en forme plus bas
+        secondsToDisplayUpAct = secondsToDisplayUpAct.toString();
+        secondsToDisplayUpInact = secondsToDisplayUpInact.toString();
+        secondsToDisplayDownAct =  secondsToDisplayDownAct.toString();
+        secondsToDisplayDownActMove = secondsToDisplayDownActMove.toString();
+        
         // on selectionne les 2 paragraphes qui contiennent chacun la moitié supérieure
-        // ou inférieure du nombre de secondes à afficher
-        let secondsFigure = document.querySelectorAll('.big-number.seconds');
-        // si le nombre déjà affiché dans les 2 paragraphes est différent de celui renvoyé 
+        // ou inférieure du nombre courant de secondes restantes à afficher
+        let secondsUpActFigureCurrentPlace = document.querySelector('.up.active .big-number.seconds');
+        let secondsDownActFigureCurrentPlace = document.querySelector('.down.active :not(.moving) .big-number.seconds');
+
+        // on selectionne respectivement les 2 paragraphes qui contiennent pour 
+        // la moitié supérieure le nombre à venir de secondes restantes à afficher
+        // ou pour l'inférieure le nombre precédant de secondes restantes à afficher
+        let secondsUpInactFigurePlace = document.querySelector('.up.inactive .big-number.seconds');
+        let secondsDownActMovFigurePlace = document.querySelector('.down.active.moving .big-number.seconds');
+
+        /* // si le nombre déjà affiché dans les 2 paragraphes est différent de celui renvoyé 
         // par la fonction
-        if (secondsFigure[0].textContent != secondsToDisplay) {
-            // on met en forme le nombre de secondes si celui-ci ne comporte qu'un caractère
-            if (secondsToDisplay.length < 2) {
-                secondsToDisplay = '0' + secondsToDisplay;
-            }
-            // on injecte le nombre de secondes entières jusqu'à la date et l'minute visées
-            secondsFigure[0].textContent = secondsToDisplay;
-            secondsFigure[1].textContent = secondsToDisplay;
+        if (secondsFigure[0].textContent != secondsToDisplay) {} */
+    
+        // on met en forme le nombre de secondes si celui-ci ne comporte qu'un caractère
+        if (secondsToDisplayUpAct.length < 2) {
+            secondsToDisplayUpAct = '0' + secondsToDisplayUpAct;
+        }
+        if (secondsToDisplayUpInact.length < 2) {
+            secondsToDisplayUpInact = '0' + secondsToDisplayUpInact;
+        }
+        if (secondsToDisplayDownAct.length < 2) {
+            secondsToDisplayDownAct = '0' + secondsToDisplayDownAct;
+        }
+        if (secondsToDisplayDownActMove.length < 2) {
+            secondsToDisplayDownActMove = '0' + secondsToDisplayDownActMove;
+        }
+        // on injecte le nombre de secondes entières jusqu'à la date et l'heure visées
+        secondsUpActFigureCurrentPlace.textContent = secondsToDisplayUpAct;
+        secondsDownActFigureCurrentPlace.textContent = 'x'; // secondsToDisplayDownAct;
+        secondsUpInactFigurePlace.textContent = secondsToDisplayUpInact;
+        secondsDownActMovFigurePlace.textContent = secondsToDisplayDownActMove;
+        
+    },
+
+    displayTimeUnitsRemaining: function(unit, upOrDown, activeOrInactive) {
+
+    },
+
+    // Flipping cards management
+
+    toggleActiveInactive: function (halfcard, active, inactive) {
+        // on active les demies cartes inactives et on inactive les actives
+        halfcard.classList.toggle(active);
+        halfcard.classList.toggle(inactive);        
+    },
+
+    swapCards: function() {
+        // on selectionne toutes les demies cartes
+        let halfCards = document.querySelectorAll('.part')
+        // on boucle la fonction qui active/inactive sur elles
+        for (let halfcard of halfCards) {
+            timer.toggleActiveInactive(halfcard, 'active', 'inactive');
         }
     },
+
+    toggleMoveActiveDownCard: function(movingDownCard) {
+        movingDownCard.classList.toggle('moving');
+    },
+
+    waitThenMoveActiveDownCards: function() {
+        // on selectionne toutes les demies cartes
+        let movingDownCards = document.querySelectorAll('.down.part.active')
+        // on boucle la fonction qui active/inactive sur elles
+        for (let movingDownCard of movingDownCards) {
+            timer.toggleMoveActiveDownCard(movingDownCard);
+        }
+    }
+
 }
+
 document.addEventListener('DOMContentLoaded', timer.init);
 console.log('JS init OK !');
 
